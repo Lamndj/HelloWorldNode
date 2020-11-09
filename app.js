e177d1ff6553e5fe162775a92efae2cf84e4966b
SURYA MAHLA <mahlasurya@gmail.com>
2020-11-09 18:02:14 +0530

    initial commit

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 6523;
 
 app.get("/", (req, res) => {
   res.send("Hey there!");
 });
 
-app.listen(port, () => console.log(`Server running at ${port}`));
+app.listen();
