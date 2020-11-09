12eda4812c317ec2c903eaaf8430ce864a813cd3
SURYA MAHLA <mahlasurya@gmail.com>
2020-11-09 18:48:25 +0530

    initial commit

--- server.js
+++ server.js
@@ -8,4 +8,8 @@ app.get("/", (req, res) => {
   res.send("THis works fine.");
 });
 
+app.get("/data", (req, res) => {
+  res.send("THis is data.");
+});
+
 app.listen(port, () => console.log(`Server running at ${port}`));
