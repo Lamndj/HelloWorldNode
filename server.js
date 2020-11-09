7bb7b7a893cb740310fcd0339e0fd65fdc56b44b
SURYA MAHLA <mahlasurya@gmail.com>
2020-11-09 19:45:27 +0530

    initial commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 6523;
 
 app.get("/", (req, res) => {
-  res.send("THis works fine.");
+  res.send("This is a dummy node js app");
 });
 
 app.get("/data", (req, res) => {
